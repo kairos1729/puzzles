2384b9515cc1a44aa26aa68cba5b66d460aa3766
kairos1729 <kairos1729@users.noreply.github.com>
2015-04-24 22:53:57 +1000

    Finished algorithm

---
+++ findcontiguous/findcontiguous/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace findcontiguous
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = new[] { 8, 1, 9, 2, 100, 91, 6, 2, 8, 4, 5 };
+
+            // O(N)
+            var h = new HashSet<int>(x);
+
+            var sequences = new List<List<int>>();
+
+            while (h.Any())
+            {
+                // O(1)
+                var current = h.First();
+                h.Remove(current);
+
+                var sequence = new List<int>() { current };
+
+                var i = current;
+                while (h.Any() && h.Contains(--i)) // O(1)
+                {
+                    h.Remove(i);
+                    sequence.Add(i);
+                }
+
+                i = current;
+                while (h.Any() && h.Contains(++i)) // O(1)
+                {
+                    h.Remove(i);
+                    sequence.Add(i);
+                }
+
+                sequences.Add(sequence);
+            }
+
+            Console.WriteLine(
+                "Longest sequence length = "
+                + sequences.Select(s => s.Count).Max());
+
+            Console.WriteLine(
+                string.Join(
+                    Environment.NewLine,
+                    sequences
+                        .OrderBy(s => s.Min())
+                        .Select(s => string.Join(", ", s.OrderBy(v => v)))));
+
+            Console.ReadLine();
+        }
+    }
+}
