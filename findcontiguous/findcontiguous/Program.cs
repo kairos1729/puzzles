d39b8fb01deac96168de1a23401d1a3c393d5ace
kairos1729 <kairos1729@users.noreply.github.com>
2015-04-24 23:41:12 +1000

    Added timings

--- findcontiguous/findcontiguous/Program.cs
+++ findcontiguous/findcontiguous/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,52 +9,66 @@ namespace findcontiguous
 {
     class Program
     {
+        private static readonly Random r = new Random();
+
         static void Main(string[] args)
         {
-            var x = new[] { 8, 1, 9, 2, 100, 91, 6, 2, 8, 4, 5 };
-
-            // O(N)
-            var h = new HashSet<int>(x);
-
-            var sequences = new List<List<int>>();
-
-            while (h.Any())
+            int n;
+            while (int.TryParse(Console.ReadLine(), out n))
             {
-                // O(1)
-                var current = h.First();
-                h.Remove(current);
+                var originalList = Enumerable.Range(0, n).Select(_ => r.Next(n));
 
-                var sequence = new List<int>() { current };
+                var stopwatch = new Stopwatch();
+                stopwatch.Start();
 
-                var i = current;
-                while (h.Any() && h.Contains(--i)) // O(1)
+                // O(N)
+                var h = new HashSet<int>(originalList);
+
+                var sequences = new List<List<int>>();
+
+                while (h.Any())
                 {
-                    h.Remove(i);
-                    sequence.Add(i);
+                    // O(1)
+                    var current = h.First();
+                    h.Remove(current);
+
+                    var sequence = new List<int>() { current };
+
+                    var i = current;
+                    while (h.Any() && h.Contains(--i)) // O(1)
+                    {
+                        h.Remove(i);
+                        sequence.Add(i);
+                    }
+
+                    i = current;
+                    while (h.Any() && h.Contains(++i)) // O(1)
+                    {
+                        h.Remove(i);
+                        sequence.Add(i);
+                    }
+
+                    sequences.Add(sequence);
                 }
 
-                i = current;
-                while (h.Any() && h.Contains(++i)) // O(1)
-                {
-                    h.Remove(i);
-                    sequence.Add(i);
-                }
+                stopwatch.Stop();
 
-                sequences.Add(sequence);
+                Console.WriteLine(string.Join(", ", originalList));
+
+                Console.WriteLine(
+                    string.Join(
+                        Environment.NewLine,
+                        sequences
+                            .OrderBy(s => s.Min())
+                            .Select(
+                                s => string.Join(", ", s.OrderBy(v => v)))));
+
+                var longest = sequences.Select(s => s.Count).Max();
+
+                Console.WriteLine(
+                    "n=" + n + " longest=" + longest +
+                    " ElapsedTicks=" + stopwatch.ElapsedTicks);
             }
-
-            Console.WriteLine(
-                "Longest sequence length = "
-                + sequences.Select(s => s.Count).Max());
-
-            Console.WriteLine(
-                string.Join(
-                    Environment.NewLine,
-                    sequences
-                        .OrderBy(s => s.Min())
-                        .Select(s => string.Join(", ", s.OrderBy(v => v)))));
-
-            Console.ReadLine();
         }
     }
 }
