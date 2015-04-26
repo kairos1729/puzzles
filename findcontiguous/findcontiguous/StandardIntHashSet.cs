763334f65a90219ce007c7958d258e421c5a064f
kairos1729 <kairos1729@users.noreply.github.com>
2015-04-26 12:51:33 +1000

    Added own hash set - makes O(x) tests more understandable

---
+++ findcontiguous/findcontiguous/StandardIntHashSet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace findcontiguous
+{
+    class StandardIntHashSet : IMyIntHashSet
+    {
+        public StandardIntHashSet(int n)
+        {
+            this.IntHashSet = new HashSet<int>(Enumerable.Range(0, n));
+            this.IntHashSet.Clear();
+        }
+
+        public StandardIntHashSet(IEnumerable<int> xs)
+        {
+            this.IntHashSet = new HashSet<int>(xs);
+        }
+
+        private HashSet<int> IntHashSet { get; set; }
+
+        public void Add(int i)
+        {
+            this.IntHashSet.Add(i);
+        }
+
+        public bool Contains(int i)
+        {
+            return this.IntHashSet.Contains(i);
+        }
+    }
+}
