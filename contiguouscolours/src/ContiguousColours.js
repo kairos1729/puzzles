55ed983813085f908b49110500241df2e1349514
kairos1729 <kairos1729@users.noreply.github.com>
2019-09-16 11:28:46 +1000

    First cut - needs testing

--- contiguouscolours/src/ContiguousColours.js
+++ contiguouscolours/src/ContiguousColours.js
@@ -8,73 +8,57 @@ const NCOL = COLOURS[0].length;
 const NROW = COLOURS.length;
 const SEEN = new Set();
 
-console.log(COLOURS[0][0]);
 colourCounts = getContiguousCounts(COLOURS);
+console.log("seen: ");
+console.log(SEEN);
+console.log(Array.from(SEEN).sort((a, b) => a - b));
+console.log("counts: ");
 console.log(colourCounts);
+
+function getContiguousCounts() {
+    const counts = new Map();
+    for (let col = 0; col < NCOL ; ++col) {
+        for (let row = 0; row < NROW; ++ row) {
+            if (!SEEN.has(coordinateKey(row, col))) {
+                const colourCount = countContiguous(row, col);
+                if (!counts.has(colourCount.colour) ||
+                    counts.get(colourCount.colour) < colourCount.count) {
+                    counts.set(colourCount.colour, colourCount.count);
+                }
+            }
+        }
+    }
+    return counts;
+}
 
 function coordinateKey(row, col) {
     return col + row * NCOL;
 }
 
-function getContiguousCounts() {
-    const counts = new Map();
-
-    for (let col = 0; col < NCOL ; ++col) {
-        for (let row = 0; row < NROW; ++ row) {
-            const colourCount = countContiguous(row, col);
-            if (!SEEN.has(coordinateKey(row, col)) &&
-                counts[colourCount.colour] < colourCount.count) {
-                counts[colourCount.colour] = colourCount.count;
-            }
+function countContiguous(row, col) {
+    const colourCount = {
+        colour: COLOURS[row][col],
+        count: 0
+    };
+    let working = [coord(row, col)];
+    while (working.length > 0) {
+        let current = working.pop();
+        if (COLOURS[current.row][current.col] !== colourCount.colour
+            || SEEN.has(coordinateKey(current.row, current.col))) {
+            continue;
         }
+        SEEN.add(coordinateKey(current.row, current.col));
+        ++colourCount.count;
+        working.push(...getAdjacentUnseen(current.row, current.col));
     }
-
-    return counts;
+    return colourCount;
 }
 
 function coord(row, col) {
     return {row: row, col: col};
 }
 
-function countContiguous(row, col) {
-    console.log(row, col);
-
-    const colourCount = {
-        colour: COLOURS[row][col],
-        count: 0
-    };
-
-    let working = [coord(row, col)];
-
-    while (working.length > 0) {
-        let current = working.pop();
-
-        console.log(current);
-
-        if (COLOURS[current.row][current.col] !== colourCount.colour) {
-            continue;
-        }
-
-        SEEN.add(coordinateKey(current.row, current.col));
-
-        ++colourCount.count;
-
-        const newSquares = getAdjacent(current.row, current.col);
-
-        console.log("new: ");
-        console.log(newSquares);
-
-        working.push(...newSquares);
-
-        console.log("working: ");
-        console.log(working);
-    }
-
-    console.log(colourCount);
-    return colourCount;
-}
-
-function getAdjacent(row, col) {
+function getAdjacentUnseen(row, col) {
     const coords = [
         coord(row - 1, col),
         coord(row + 1, col),
@@ -82,18 +66,9 @@ function getAdjacent(row, col) {
         coord(row, col + 1),
     ];
 
-    console.log("coords: ");
-    console.log(coords);
-
-    const filtered = coords.filter(c =>
+    return coords.filter(c =>
         c.row >= 0 &&
         c.row < NROW &&
         c.col >= 0 &&
-        c.col < NCOL &&
-        !SEEN.has(coordinateKey(c.row, c.col)));
-
-    console.log("filtered: ");
-    console.log(filtered);
-
-    return filtered;
+        c.col < NCOL);
 }
